38f8b90a1b38d0a3dc7a7c806cc2b720deb1d6c4
Hilton Medeiros <medeiros.hilton@gmail.com>
2012-07-04 17:10:31 -0300

    Update allegro cmake module to use library suffixes.

--- cmake/FindAllegro.cmake
+++ cmake/FindAllegro.cmake
@@ -5,6 +5,16 @@
 #  ALLEGRO_LIBRARIES   - List of libraries when using Allegro.
 #  ALLEGRO_FOUND       - True if libOgreMain found.
 
+function(append_lib_type_suffix var)
+    string(TOLOWER "${CMAKE_BUILD_TYPE}" CMAKE_BUILD_TYPE_TOLOWER)
+    if(CMAKE_BUILD_TYPE_TOLOWER STREQUAL "debug")
+        set(${var} "${${var}}-debug" PARENT_SCOPE)
+    endif(CMAKE_BUILD_TYPE_TOLOWER STREQUAL "debug")
+    if(CMAKE_BUILD_TYPE_TOLOWER MATCHES "profile")
+        set(${var} "${${var}}-profile" PARENT_SCOPE)
+    endif(CMAKE_BUILD_TYPE_TOLOWER MATCHES "profile")
+endfunction(append_lib_type_suffix)
+
 if(ALLEGRO_INCLUDE_DIR)
     # Already in cache, be silent
     set(ALLEGRO_FIND_QUIETLY TRUE)
@@ -12,10 +22,19 @@ endif(ALLEGRO_INCLUDE_DIR)
 
 find_path(ALLEGRO_INCLUDE_DIR allegro5/allegro.h)
 
-find_library(ALLEGRO_LIBRARY NAMES allegro)
-find_library(ALLEGRO_IMAGE_LIBRARY NAMES allegro_image)
-find_library(ALLEGRO_FONT_LIBRARY NAMES allegro_font)
-find_library(ALLEGRO_PRIMITIVES_LIBRARY NAMES allegro_primitives)
+set (ALLEGRO_NAME allegro)
+set (ALLEGRO_IMAGE_NAME allegro_image)
+set (ALLEGRO_FONT_NAME allegro_font)
+set (ALLEGRO_PRIMITIVES_NAME allegro_primitives)
+append_lib_type_suffix(ALLEGRO_NAME)
+append_lib_type_suffix(ALLEGRO_IMAGE_NAME)
+append_lib_type_suffix(ALLEGRO_FONT_NAME)
+append_lib_type_suffix(ALLEGRO_PRIMITIVES_NAME)
+
+find_library(ALLEGRO_LIBRARY NAMES ${ALLEGRO_NAME})
+find_library(ALLEGRO_IMAGE_LIBRARY NAMES ${ALLEGRO_IMAGE_NAME})
+find_library(ALLEGRO_FONT_LIBRARY NAMES ${ALLEGRO_FONT_NAME})
+find_library(ALLEGRO_PRIMITIVES_LIBRARY NAMES ${ALLEGRO_PRIMITIVES_NAME})
 
 # Handle the QUIETLY and REQUIRED arguments and set ALLEGRO_FOUND to TRUE if
 # all listed variables are TRUE.
